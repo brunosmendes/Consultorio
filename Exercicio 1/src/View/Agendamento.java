a3e7ceba8b3459835de6596878a9b7680a50b4ed
Juliana Paz <julianapaz07@hotmail.com>
2014-08-12 11:37:36 -0300

    Modificacoes na data

--- Exercicio 1/src/View/Agendamento.java
+++ Exercicio 1/src/View/Agendamento.java
@@ -1,6 +1,10 @@
 package View;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Agendamento {
 Scanner leia = new Scanner(System.in);
@@ -13,6 +17,17 @@ Scanner leia = new Scanner(System.in);
 	}
 	//________________________________________________
 	public String dataAgendamento(){
+		Date data = new Date();
+		   SimpleDateFormat formatador = new SimpleDateFormat("dd/mm/yyyy hh:mm");
+		   String dataString = 
+		  
+		 
+		String dataString = JOptionPane.showInputDialog("Digite a data:");
+		Date data1 = formatador.parse(dataString);
+		System.out.println(data1);
+		JOptionPane.showMessageDialog(null, formatador.format(data));
+		
+		
 		System.out.println("informe a data:");
 		String data = leia.next();
 		return data;
